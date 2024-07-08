90f418128318b6c05391b6febe1a064a214b88c9
Carlos Zabala <czabala847@gmail.com>
2024-07-08 16:54:51 -0500

    add usequery

--- src/api/ProjectApi.ts
+++ src/api/ProjectApi.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { ProjectFormData } from "../types";
+import { IProject, ProjectFormData } from "../types";
 
 export async function createProject(data: ProjectFormData) {
   try {
@@ -12,5 +12,18 @@ export async function createProject(data: ProjectFormData) {
     }
 
     throw new Error("Error al crear el proyecto");
+  }
+}
+
+export async function getProjects() {
+  try {
+    const { data: response } = await api.get<IProject[]>("/projects");
+    return response;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+
+    throw new Error("Error al obtener los proyectos");
   }
 }
